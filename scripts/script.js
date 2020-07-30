2e0399ef4a42ba296ff3ff3fb48a0909f5887d95
DomeDaniel <dome.daniel.144@gmail.com>
2020-07-30 21:56:14 +0200

    blabla

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,22 @@ function calcAmount() {
     let amountInput = document.querySelector("input[name='amount-input']");
     let showAmount = document.querySelector("span.show-amount");
     let amountNumber = parseInt(amountInput.value);
+
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+
     if (amountNumber > 10) {
         alert("Maximum 10 terméket vásárolhat!");
         return
     } else if (amountNumber < 1) {
-        alert("Minimu 1 terméket kell vásárolnia!");
+        alert("Minimum 1 terméket kell vásárolnia!");
     } else {
-        let amount = parseInt(amountInput.value) * price;
+        let amount = amountNumber * price;
         showAmount.innerHTML = amount;
     }
-
 }
+ /*
+function hider() {
+    document.body.innerHTML = "";
+    document.head.innerHTML = "";
+}
+*/
